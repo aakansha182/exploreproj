931189c20bf5eec047e3e045c2ad5b1ee7aa21cc
aakansha182 <155164854+aakansha182@users.noreply.github.com>
2024-02-19 21:26:52 +0530

    comit

--- client/src/components/UserProfile/YourBooks.jsx
+++ client/src/components/UserProfile/YourBooks.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const YourBooks = () => {
+function YourBooks() {
   return (
     <div>YourBooks</div>
   )
--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ mongoose.connect(
   // "mongodb+srv://exp:exp123@clusterexp.xw5sehz.mongodb.net/session-exp?retryWrites=true&w=majority",
   //"mongodb+srv://exp:explore@explorecluster.yweprwi.mongodb.net/expdb?retryWrites=true&w=majority",
   // "mongodb+srv://exp:<password>@cluster0.wpeuved.mongodb.net/?retryWrites=true&w=majority",
- "mongodb+srv://exp:exp@cluster0.wpeuved.mongodb.net/session-exp?retryWrites=true&w=majority",
+ //"mongodb+srv://exp:exp@cluster0.wpeuved.mongodb.net/session-exp?retryWrites=true&w=majority",
  "mongodb+srv://exp:exp@cluster0.wpeuved.mongodb.net/session-exp?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
